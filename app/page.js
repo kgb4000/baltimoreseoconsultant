32d7fbbdf505aa00e2c7fecc8309fae30c98b13c
kgb <kester4000@gmail.com>
2023-06-24 15:26:31 -0400

    Added more content.

--- app/page.js
+++ app/page.js
@@ -6,8 +6,14 @@ export default function Home() {
     <main>
       <header>
         <div className="container">
-          <p>Kester Browne</p>
-          <p>Baltimore SEO Consultant</p>
+          <img
+            src="/images/logo.webp"
+            className="logo"
+            alt="Baltimore SEO Consultant"
+            title="Baltimore SEO Consultant"
+            width="200px"
+            heoght="auto"
+          />
           <nav>
             <ul className="nav">
               <li>
@@ -42,16 +48,26 @@ export default function Home() {
       <section>
         <div className="container">
           <h2 className="title">
-            5 Reasons You Need An SEO Consultant in Baltimore, MD
+            5 Reasons to Work With An SEO Consultant in Baltimore
           </h2>
           <div>
             <h3>#1. Your Competitors are Already Doing SEO</h3>
+            <p>
+              You see it every time you search for your services. Your
+              competitors on the first page. Don’t believe me? Search for one of
+              your services and see who comes up first, second, or third. It’s
+              not by luck or by accident. They are doing search engine
+              optimization and have an SEO company or expert doing SEO on their
+              website. They rank the keywords that people use to find the
+              services you offer.
+            </p>
+            <p>This brings us to the second reason.</p>
           </div>
           <div>
-            <h3>#2. Get Leads Allday Everyday</h3>
+            <h3>#2. You Gain a Competitive advantage</h3>
           </div>
           <div>
-            <h3>#3. You Gain a Competitive advantage</h3>
+            <h3>#3. Get Leads Allday Everyday</h3>
           </div>
           <div>
             <h3>#4. More People Will See Your Business on Google</h3>
@@ -64,7 +80,7 @@ export default function Home() {
       </section>
       <section id="services">
         <div className="container">
-          <h2 className="title">Our Baltimore SEO Services</h2>
+          <h2 className="title">My Baltimore SEO Services</h2>
           <p className="subtext">
             Here are the search engine optimization services I give to Baltimore
             businesses to succeed online
@@ -157,17 +173,51 @@ export default function Home() {
           <div>
             <h3>I Focus on the Results</h3>
             <p>
-              Getting traffic to your website is nice, but are you making money.
+              As an SEO specialist, my primary focus is on delivering tangible
+              results that drive your business forward.
             </p>
             <p>
-              Are you getting more phone calls and more leads? Are you getting
-              more sales? Are you getting a return on your SEO investment.
+              I understand that SEO is not just about rankings and traffic, but
+              about generating leads, increasing sales, and growing your bottom
+              line. My strategies are designed with this end goal in mind,
+              seeking not just to improve your website's visibility, but to
+              ensure this visibility leads to conversions.
             </p>
-            <p>That's what I care about.</p>
-            <p>Everything else doesn't really matter.</p>
+            <p>
+              I monitor and analyze a variety of metrics to gauge the
+              effectiveness of my work, adjusting strategies as needed to
+              maximize results. Regular reports keep you informed about these
+              results, clearly illustrating the return on your investment.
+            </p>
+            <p>
+              I'm committed to holding myself accountable for your success and
+              will not rest until your SEO campaign delivers the impact it
+              should.
+            </p>
           </div>
           <div>
             <h3>I Build Custom SEO Campaigns</h3>
+            <p>
+              I specialize in creating tailor-made SEO campaigns that cater to
+              the unique needs of each business.
+            </p>
+            <p>
+              Instead of one-size-fits-all approaches, I believe in curating
+              strategies that reflect your specific goals, industry, and target
+              audience.
+            </p>
+            <p>
+              By thoroughly understanding your business and its online
+              objectives, I ensure that every aspect of the campaign, from
+              keyword selection to content creation, is customized to maximize
+              your online visibility. I also focus on ongoing optimization,
+              routinely assessing and refining the strategy based on analytics
+              and performance data.
+            </p>
+            <p>
+              This personalized approach allows me to drive more organic traffic
+              to your website and improve its search engine ranking.
+            </p>
           </div>
           <div>
             <h3>I'm a Real Baltimore SEO Expert</h3>
@@ -175,23 +225,45 @@ export default function Home() {
           <div>
             <h3>I am 100% Transparent</h3>
             <p>
-              You will know everything I'm doing. You can call or email me with
-              any questions or concerns.
+              I firmly believe in keeping clients fully informed about their SEO
+              campaigns at every stage, explaining not just what I'm doing, but
+              why I'm doing it. I provide regular, detailed reports showcasing
+              the results of my work, including key performance indicators,
+              metrics, and achieved milestones.
             </p>
             <p>
-              If you want to learn how to do SEO yourself, I'm down to teach you
-              my process.
+              I am also upfront about any challenges encountered and how I plan
+              to overcome them. In every communication, my goal is to make the
+              complex world of SEO understandable and accessible. This way,
+              you're not just a spectator, but a knowledgeable participant in
+              your own SEO strategy. Being 100% transparent means building trust
+              and fostering a collaborative, honest relationship with my
+              clients.
             </p>
           </div>
           <div>
             <h3>I Never Work With Your Competition</h3>
             <p>
-              Some SEO companies will work with anybody. Even your competiton in
-              the same city.
+              My policy is simple and straightforward: I never work with your
+              competition.
             </p>
-            <p>That's not right.</p>
-            <p>When we work together, I only work for you in your industry in your city.</p>
-            <p>I'll never try to help the competiton, and I have rules and scuples I live by.</p>
+            <p>
+              I strongly believe in providing my clients with exclusive
+              services, and this means dedicating my skills and resources to a
+              single business per industry in a given market area. This
+              principle ensures that I am fully invested in your success,
+              without any conflicting interests.
+            </p>
+            <p>
+              It also guarantees that the strategies, insights, and competitive
+              advantages I develop for your business are unique to you and not
+              shared with your competitors.
+            </p>
+            <p>
+              I am committed to this principle of exclusivity as it enables me
+              to focus completely on helping your business dominate your
+              industry's search engine results.
+            </p>
           </div>
           <Button>Book Your Strategy Call Today!</Button>
         </div>
@@ -201,12 +273,138 @@ export default function Home() {
           <h2>Frequently Asked Questions about Baltimore SEO</h2>
           <div>
             <h3>How much does SEO cost for a business in Baltimore?</h3>
+            <p>Well how much is ti costing you by not doing SEO?</p>
+            <p>Let's see</p>
+            <ul className="faq-list">
+              <li>
+                <h4>Low Online Visibility and Presence</h4>
+                <p>
+                  Without SEO, your website will have a harder time appearing in
+                  search results, leading to fewer visitors and, ultimately,
+                  fewer conversions.
+                </p>
+              </li>
+              <li>
+                <h4>Decreased Credibility</h4>
+                <p>
+                  SEO also involves making sure your website is easily navigable
+                  and user-friendly. If your site is hard to use, visitors may
+                  perceive your business as less credible.
+                </p>
+              </li>
+              <li>
+                <h4>Missed Customer Acquisition Opportunities</h4>
+                <p>
+                  People use search engines to find solutions to their problems.
+                  If your business isn't visible when potential customers are
+                  searching for what you offer, you're missing out on a major
+                  customer acquisition channel.
+                </p>
+              </li>
+              <li>
+                <h4>Lower ROI</h4>
+                <p>
+                  Compared to paid advertising, SEO typically provides a much
+                  higher return on investment in the long run. While SEO
+                  requires ongoing effort, the organic traffic it brings often
+                  converts better than traffic from paid ads.
+                </p>
+              </li>
+              <li>
+                <h4>Competitive Disadvantage</h4>
+                <p>
+                  If your competitors are leveraging SEO and you're not, they'll
+                  likely win the organic search visibility, resulting in more
+                  traffic and potential revenue for them instead of you.
+                </p>
+              </li>
+            </ul>
           </div>
           <div>
             <h3>Should you work with a large digital agency?</h3>
+            <p>You can, be you may experience the following:</p>
+            <ul className="faq-list">
+              <li>
+                <h4>Less Personal Attention</h4>
+                <p>
+                  Larger SEO agencies in Baltimore, your account might be one
+                  among hundreds or even thousands. As a result, you might not
+                  receive the personalized service or attention to detail that a
+                  smaller agency or freelancer could provide. This could lead to
+                  your business's unique needs being overlooked or not
+                  adequately addressed.
+                </p>
+              </li>
+              <li>
+                <h4>Risk of Being 'Just a Number'</h4>
+                <p>
+                  In a large agency, there's a risk of feeling like just another
+                  client on a list, rather than a valued partner. This can
+                  impact the level of personal care and urgency applied to your
+                  business.
+                </p>
+              </li>
+              <li>
+                <h4>High Employee Turnover</h4>
+                <p>
+                  Large agencies often have higher employee turnover rates,
+                  which can impact the consistency and quality of your SEO
+                  services. If your account manager leaves, for example, it
+                  could take time for their replacement to get up to speed on
+                  your account, resulting in potential setbacks.
+                </p>
+              </li>
+              <li>
+                <h4>Cost</h4>
+                <p>
+                  Larger SEO agencies often have higher overhead costs, which
+                  can translate into higher prices for their clients. If you're
+                  a small business or startup, these costs might be beyond your
+                  budget.
+                </p>
+              </li>
+              <li>
+                <h4>Longer Response Times</h4>
+                <p>
+                  With many clients to serve, larger agencies may have slower
+                  response times to questions, concerns, or requests. You might
+                  have to wait longer for replies or action on your account.
+                </p>
+              </li>
+              <li>
+                <h4>Standardized Packages</h4>
+                <p>
+                  Larger SEO agencies often work off of standardized packages
+                  and strategies, which might not be the best fit for every
+                  business. Your company's unique needs might require a more
+                  tailored approach.
+                </p>
+              </li>
+              <li>
+                <h4>Pricey Services</h4>
+                <p>
+                  Large SEO agencies often have higher overhead costs due to
+                  their size, sophisticated tools, and wide range of staff
+                  specializations. These costs can translate into higher prices
+                  for their services compared to smaller agencies or
+                  freelancers.
+                </p>
+              </li>
+              <li>
+                <h4>No Transparency</h4>
+                <p>
+                  Sometimes, large SEO agencies can be less transparent about
+                  their strategies and techniques. This can make it difficult
+                  for you to understand what work is being done and how
+                  effective it is.
+                </p>
+              </li>
+            </ul>
           </div>
           <div>
-            <h3>How long does it take to rank my website?</h3>
+            <h3>Can You Gaurantee #1 Rankings in Google?</h3>
+            <p>No.</p>
+            <p>No one can.</p>
           </div>
         </div>
       </section>
