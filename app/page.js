28e93e75de095c3db4ba2b7a729734da628f84b5
kgb <kester4000@gmail.com>
2023-06-25 17:05:20 -0400

    Added more content.

--- app/page.js
+++ app/page.js
@@ -259,6 +259,7 @@ export default function Home() {
               <img
                 src="/images/one-on-one-consulting.png"
                 className="icon"
+                alt="One on one consulting."
                 loading="lazy"
               />
               <div>
